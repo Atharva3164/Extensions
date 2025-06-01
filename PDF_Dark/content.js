cf79bca8b6adc87b723e37b8c29419a41f80c7a2
Atharva Shinde <atharvacodefiles@gmail.com>
2025-06-01 23:31:28 +0530

    Delete PDF_Dark directory

--- PDF_Dark/content.js
+++
@@ -1,5 +0,0 @@
-chrome.storage.local.get(["invertState"], (data) => {
-    let isInverted = data.invertState ?? false; // Use default if undefined
-    let embed = document.querySelector("embed");
-    if (embed) embed.style.filter = isInverted ? "invert(1)" : "none";
-});
